9666f779db2d04839aec81dcb3e0d6053d05a577
DontNeg <lukianmokahel123@gmail.com>
2024-11-30 16:01:42 -0800

    Desc
    - Updated Relevant dependencies
    - Cleaned up "cleanUp" class

--- untitled/src/main/java/millllionWith4Ls/bot/Main/commands/cleanUp.java
+++ untitled/src/main/java/millllionWith4Ls/bot/Main/commands/cleanUp.java
@@ -6,36 +6,32 @@ public class cleanUp {
         return "```ansi\n\u001b[" + backgroundColor(bgColor) + ";" + mainColor(color) + "m" + text + "\n```";
     }
     public static int backgroundColor(String backgroundColor){
-        int rBackgroundColor;
-        switch(backgroundColor.toLowerCase()){
-            case "dark blue" -> rBackgroundColor = 40;
-            case "orange" -> rBackgroundColor = 41;
-            case "dark gray" -> rBackgroundColor = 43;
-            case "gray" -> rBackgroundColor = 44;
-            case "purple" -> rBackgroundColor = 45;
-            case "light gray" -> rBackgroundColor = 46;
-            case "white" -> rBackgroundColor =47;
-            default -> rBackgroundColor = 0;
-        }
-        return rBackgroundColor;
+        return switch(backgroundColor.toLowerCase()){
+            case "dark blue" -> 40;
+            case "orange" -> 41;
+            case "dark gray" -> 43;
+            case "gray" -> 44;
+            case "purple" -> 45;
+            case "light gray" -> 46;
+            case "white" -> 47;
+            default -> 0;
+        };
     }
     public static int mainColor(String color){
-        int rColor;
-        switch(color.toLowerCase()){
-            case "gray" -> rColor = 30;
-            case "red" -> rColor = 31;
-            case "green" -> rColor = 32;
-            case "orange" -> rColor = 33;
-            case "blue" -> rColor = 34;
-            case "pink" -> rColor = 35;
-            case "cyan" -> rColor =36;
-            case "white" -> rColor =37;
-            default -> rColor = 0;
-        }
-        return rColor;
+        return switch(color.toLowerCase()){
+            case "gray" -> 30;
+            case "red" -> 31;
+            case "green" -> 32;
+            case "orange" -> 33;
+            case "blue" -> 34;
+            case "pink" -> 35;
+            case "cyan" -> 36;
+            case "white" -> 37;
+            default -> 0;
+        };
     }
     private static String mainModifier(String msg, String modifier) {
-        return switch (modifier.toLowerCase()) {
+        return switch(modifier.toLowerCase()) {
             case "bold" -> "**" + msg + "**";
             case "italic" -> "*" + msg + "*";
             case "strikethrough" -> "~~" + msg + "~~";
